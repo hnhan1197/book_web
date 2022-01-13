f1c13b6a7b2d52ceeefc5027cc8b4d5d074f2133
Hoang Nhan <hnhan1197@gmail.com>
2022-01-13 14:18:01 +0700

    list books

---
+++ Controllers/BookController.cs
@@ -0,0 +1,27 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private List<Book> listBooks;
+
+        public BookController()
+        {
+            listBooks = new List<Book>();
+            listBooks.Add(new Book(1, "Sách 1", "Tác giả 1", 2017, 500000,"Content/img/book1.png"));
+            listBooks.Add(new Book(2, "Sách 2", "Tác giả 2", 2013, 700000, "Content/img/book2.jpg"));
+            listBooks.Add(new Book(3, "Sách 3", "Tác giả 3", 2019, 100000, "Content/img/book3.jpg"));
+        }
+        // GET: Book
+        public ActionResult ListBooks()
+        {
+            return View(listBooks);
+        }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Models
+{
+    public class Book
+    {
+        public Book(int iD, string tittle, string author, int publishYear, double price, string cover)
+        {
+            this.iD = iD;
+            this.tittle = tittle;
+            this.author = author;
+            this.publishYear = publishYear;
+            this.price = price;
+            this.cover = cover;
+        }
+
+
+        public int iD { get; set; }
+        public string tittle { get; set; }
+        public string author { get; set; }
+        public int publishYear { get; set; }
+        public double price { get; set; }
+        public string cover { get; set; }
+    }
+}
